52476aebe0b01948a2a53e25d8c8b85adb74df36
Steve Lanier <stevekentlanier@gmail.com>
2019-03-22 10:17:43 -0700

    Questions answered, styling added, and home world removed from form display.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -8,35 +8,35 @@ const Character = (props) => {
       <div>
         <div>
           <h3>Birth Year:</h3> 
-          <span>{props.obj.birth_year}</span>
+          <span class="cool-text">{props.obj.birth_year}</span>
         </div>       
-        <div>
+        {/* <div>
           <h3>Home World:</h3> 
           <span>{props.obj.homeworld}</span>
-        </div>
+        </div> */}
         <div>
           <h3>Eye Color:</h3>
-          <span >{props.obj.eye_color}</span>
+          <span class="cool-text">{props.obj.eye_color}</span>
         </div>
         <div>
           <h3>Gender:</h3>
-          <span>{props.obj.gender}</span>
+          <span class="cool-text">{props.obj.gender}</span>
         </div>
         <div>
           <h3>Hair Color:</h3>
-          <span>{props.obj.hair_color}</span>
+          <span class="cool-text">{props.obj.hair_color}</span>
         </div>
         <div>
           <h3>Height:</h3>
-          <span>{props.obj.height}</span>
+          <span class="cool-text">{props.obj.height}</span>
         </div>
         <div>
           <h3>Mass:</h3>
-          <span>{props.obj.mass}</span>
+          <span class="cool-text">{props.obj.mass}</span>
         </div>
         <div>
           <h3>Skin Color:</h3>
-          <span>{props.obj.skin_color}</span>
+          <span class="cool-text">{props.obj.skin_color}</span>
         </div>
       </div>
     </div>
